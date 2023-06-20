255dcff05a180f4c44052d0fac120cefb328454f
Martyn Nevers <martyn@esdet.com>
2023-06-20 15:11:24 +0100

    Use multiple assert block

--- PleaseWait.Tests/UnitTests.cs
+++ PleaseWait.Tests/UnitTests.cs
@@ -36,13 +36,16 @@ namespace PleaseWait.Tests
             var pollInterval = wait.GetFieldValue<TimeSpan>("pollInterval");
             var prereqs = wait.GetFieldValue<IList<Action>>("prereqs");
             var alias = wait.GetFieldValue<IList<Action>>("alias");
-            Assert.That(shouldFailSilently, Is.False);
-            Assert.That(shouldIgnoreExceptions, Is.True);
-            Assert.That(timeout.TotalSeconds, Is.EqualTo(10));
-            Assert.That(pollDelay.TotalMilliseconds, Is.EqualTo(100));
-            Assert.That(pollInterval.TotalMilliseconds, Is.EqualTo(100));
-            Assert.Null(prereqs);
-            Assert.Null(alias);
+            Assert.Multiple(() =>
+            {
+                Assert.That(shouldFailSilently, Is.False);
+                Assert.That(shouldIgnoreExceptions, Is.True);
+                Assert.That(timeout.TotalSeconds, Is.EqualTo(10));
+                Assert.That(pollDelay.TotalMilliseconds, Is.EqualTo(100));
+                Assert.That(pollInterval.TotalMilliseconds, Is.EqualTo(100));
+                Assert.That(prereqs, Is.Null);
+                Assert.That(alias, Is.Null);
+            });
         }
 
         [Test]
@@ -56,9 +59,12 @@ namespace PleaseWait.Tests
             var timeout = wait.GetFieldValue<TimeSpan>("timeout");
             var pollDelay = wait.GetFieldValue<TimeSpan>("pollDelay");
             var pollInterval = wait.GetFieldValue<TimeSpan>("pollInterval");
-            Assert.That(timeout.TotalDays, Is.EqualTo(1));
-            Assert.That(pollDelay.TotalHours, Is.EqualTo(2));
-            Assert.That(pollInterval.TotalMinutes, Is.EqualTo(3));
+            Assert.Multiple(() =>
+            {
+                Assert.That(timeout.TotalDays, Is.EqualTo(1));
+                Assert.That(pollDelay.TotalHours, Is.EqualTo(2));
+                Assert.That(pollInterval.TotalMinutes, Is.EqualTo(3));
+            });
         }
 
         [Test]
@@ -177,8 +183,11 @@ namespace PleaseWait.Tests
                 .With().Prereqs(prereqs)
                 .Until(() => orange.IsPeeled);
 
-            Assert.IsTrue(toggle);
-            Assert.That(i, Is.GreaterThan(0));
+            Assert.Multiple(() =>
+            {
+                Assert.That(toggle, Is.True);
+                Assert.That(i, Is.GreaterThan(0));
+            });
         }
 
         [Test]
